10884ab8ca14d0cd89709d955296c5ffb8f5f91a
ubungmeister <ubungmeister@gmail.com>
2025-01-28 13:06:00 +0100

    image size fixed

--- src/app/products/components/SingleProduct.tsx
+++ src/app/products/components/SingleProduct.tsx
@@ -21,13 +21,13 @@ export const SingleProduct = ({ product }: ProductProps) => {
     >
       <div className="w-[300px] h-[300px] mx-auto justify-center items-center space-y-4">
         <p className="text-center">{product.title}</p>
-        <div className="w-[150px] h-[150px] mx-auto">
+        <div className="w-[200px] h-[200px] mx-auto flex justify-center items-center overflow-hidden">
           <Image
             src={product.image}
             alt={product.title}
             width={150}
             height={150}
-            className="object-cover"
+            className="w-full h-full object-contain"
           />
         </div>
       </div>
